3dd33d1a67817602e612ac2c79fad090fd08c3ab
TenSlime <alitpurnama13@gmail.com>
2024-02-05 19:51:13 +0900

    init typing minigame

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,12 @@
   import MemoryCard from "./lib/MemoryCard.svelte";
   import { ChevronRight } from "lucide-svelte";
   import Navigation from "./lib/Navigation.svelte";
+  import TypingMinigame from "./lib/TypingMinigame.svelte";
 </script>
 
-<main class="h-screen w-screen bg-gray-800 text-white font-bebas tracking-wide">
+<main
+  class="h-screen w-screen bg-gray-800 text-white font-bebas tracking-wide overflow-hidden"
+>
   <Navigation />
-  <MemoryCard />
+  <TypingMinigame />
 </main>
---
+++ src/lib/TypingMinigame.svelte
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos non, repellendus
+perferendis nostrum saepe ab sequi possimus dolor neque. Voluptatem ex quas
+nostrum provident quae ab, itaque illum iste exercitationem.
