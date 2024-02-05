0260f8cced14fa5314addd4b684551802d908b5a
TenSlime <alitpurnama13@gmail.com>
2024-02-05 19:50:52 +0900

    feat: check match function & more
    - add check if both flipped card is matched
    - add function to check if everything already matched (game end)
    - add flip animation transition

--- src/lib/MemoryCard.svelte
+++ src/lib/MemoryCard.svelte
@@ -4,16 +4,21 @@
   import type { CardType } from "../types";
   import { flip } from "svelte/animate";
   import { quadInOut } from "svelte/easing";
+  import { fade } from "svelte/transition";
 
+  let OpenCard1: CardType | null = null;
+  let OpenCard2: CardType | null = null;
+
+  // NOTE: Use nerd font so you can see the symbol below
   let SymbolList = [
     // Arch
     " 󰣇 ",
-    // Gamepad
-    " 󰮂 ",
     // Krita
     "  ",
     // Linux
     "  ",
+    // Ubuntu
+    "  ",
     // Gentoo
     "  ",
     // Garuda
@@ -32,14 +37,46 @@
     "  ",
     // Gnome
     "  ",
-    // Xfce
-    "  ",
+    // Rust
+    "  ",
+    // Typescript
+    " 󰛦 ",
+    // Javscript
+    " 󰌞 ",
   ];
 
+  // TODO: Rework DoFlip and ResetFlip function must be the stupidiest thing I've ever done
+  function DoFlip(id: string) {
+    if ((OpenCard1 && OpenCard2) !== null) return;
+    if (!OpenCard1) {
+      console.log("DOFLIP");
+      OpenCard1 = $MemoryStore.find((x) => x.id === id)!;
+      return;
+    } else if (!OpenCard2) {
+      OpenCard2 = $MemoryStore.find((x) => x.id === id)!;
+      return;
+    }
+  }
+
+  function ResetFlip(id: string) {
+    if (OpenCard1 && OpenCard1.id === id) {
+      OpenCard1 = null;
+    } else if (OpenCard2 && OpenCard2.id === id) {
+      OpenCard2 = null;
+    }
+  }
+
   function ClickCard(id: string) {
+    if (OpenCard1 && OpenCard2) return;
     let Data = $MemoryStore.find((x) => x.id === id);
     if (!Data) return;
+    if (Data.matched) return;
     Data.flipped = !Data.flipped;
+    if (Data.flipped) {
+      DoFlip(id);
+    } else {
+      ResetFlip(id);
+    }
     MemoryStore.update((x) => {
       let data = x.find((y) => y.id === id);
       if (data) {
@@ -56,7 +93,6 @@
     while (currentIndex > 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex],
         array[currentIndex],
@@ -65,6 +101,7 @@
     return array;
   }
 
+  // NOTE: Doesn't guarantee to be random (might be duplicated)
   function GenerateID() {
     return Math.random()
       .toString(36)
@@ -75,11 +112,47 @@
     return SymbolList[Math.floor(Math.random() * SymbolList.length)];
   }
 
+  function CheckIsMatch() {
+    if (!(OpenCard1 && OpenCard2)) return;
+    if (OpenCard1.emoji === OpenCard2.emoji) {
+      MemoryStore.update((x) => {
+        let card1 = $MemoryStore.find((y) => y.id === OpenCard1?.id);
+        let card2 = $MemoryStore.find((y) => y.id === OpenCard2?.id);
+        if (card1 && card2) {
+          card1.matched = true;
+          card2.matched = true;
+          ResetFlip(card1.id);
+          ResetFlip(card2.id);
+        }
+        return x;
+      });
+    } else {
+      setTimeout(() => {
+        MemoryStore.update((y) => {
+          let filtered = y.filter((x) => x.flipped);
+          filtered.forEach((x) => {
+            ResetFlip(x.id);
+            ResetFlip(x.id);
+            x.flipped = false;
+          });
+          return y;
+        });
+      }, 500);
+    }
+  }
+
+  function IsAllMatch() {
+    const MatchedData = $MemoryStore.filter((x) => x.matched);
+    if (MatchedData.length !== $MemoryStore.length) return;
+    return true;
+  }
+
   function GenerateData() {
     MemoryStore.set([]);
     let emojis: String[] = [];
     for (let i = 0; i < 8; i++) {
       let emoji: string = GetEmoji();
+      // Just make sure the emoji doesn't get duplicated
       while (emojis.find((x) => x === emoji) !== undefined) {
         emoji = GetEmoji();
       }
@@ -92,43 +165,61 @@
         ];
       });
     }
-    setTimeout(() => {
-      MemoryStore.set(ShuffleArray($MemoryStore));
-    }, 3000);
+    MemoryStore.set(ShuffleArray($MemoryStore));
   }
   onMount(GenerateData);
+  $: [OpenCard1, OpenCard2], CheckIsMatch();
+  $: $MemoryStore, IsAllMatch();
 </script>
 
+<pre>
+  {JSON.stringify(OpenCard1)}
+  {JSON.stringify(OpenCard2)}
+</pre>
 <div class="memory-card-wrapper">
-  <div>
-    <h1 class="text-4xl text-center">Memory Card</h1>
-    <div class="grid place-items-center">
-      <div
-        class="border border-blue-200 rounded mt-5 p-2 grid grid-cols-4 gap-2 w-fit"
-      >
-        {#each $MemoryStore as { id, emoji, matched, flipped } (id)}
-          <button
-            class="card"
-            animate:flip={{ duration: 400, easing: quadInOut }}
-            class:flipped
-            on:click={() => {
-              ClickCard(id);
-            }}
-            >{flipped || matched ? emoji : "?"}
-          </button>
-        {/each}
-      </div>
+  <div class="relative grid place-items-center overflow-hidden">
+    <div
+      class="border border-blue-200 rounded mt-5 p-2 grid grid-cols-4 gap-2 w-fit"
+    >
+      {#each $MemoryStore as { id, emoji, matched, flipped } (id)}
+        <button
+          class="card"
+          animate:flip={{ duration: 400, easing: quadInOut }}
+          class:flipped={flipped || matched}
+          on:click={() => {
+            ClickCard(id);
+          }}
+        >
+          <div class="back">
+            {emoji}
+          </div>
+        </button>
+      {/each}
     </div>
   </div>
 </div>
 
 <style lang="postcss">
+  .card > div {
+    backface-visibility: hidden;
+    position: absolute;
+    @apply w-full h-full flex justify-center items-center;
+  }
+  .card > .back {
+    transform: rotateY(180deg);
+    @apply bg-blue-300 font-nerdsymbol;
+  }
   .card.flipped {
     @apply font-nerdsymbol;
   }
   .card {
     @apply bg-blue-200 text-gray-900 transition-all ease-in-out;
     @apply lg:w-32 lg:h-32 md:h-24 md:w-24 grid place-items-center text-4xl;
+    @apply relative duration-700;
+    transform-style: preserve-3d;
+  }
+  .card.flipped {
+    transform: rotateY(180deg);
   }
   .memory-card-wrapper {
     @apply w-screen h-screen grid place-items-center;
